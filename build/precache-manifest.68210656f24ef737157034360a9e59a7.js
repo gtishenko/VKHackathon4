148b09829104c90fd6d8fd2993b4faadaec0fec2
GRAT <gregorytyshchenko3011@gmail.com>
2020-09-14 15:57:25 +0300

    first commit

---
+++ build/precache-manifest.68210656f24ef737157034360a9e59a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99ea2ebdbdba201b32a5657abb77993",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "3277ea5618d870f806bc",
+    "url": "/static/js/2.82a9c8b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.82a9c8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a101d27f8864bd94b5",
+    "url": "/static/js/main.ae14370c.chunk.js"
+  },
+  {
+    "revision": "2f729f8dfb644b1198eb",
+    "url": "/static/js/runtime-main.4146942f.js"
+  }
+]);
